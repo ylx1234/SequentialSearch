8885306b5076f1c2e3462943906d4839ee85f50d
ylx1234 <46913518+ylx1234@users.noreply.github.com>
2020-03-16 20:12:44 +0800

    Add files via upload

---
+++ SequentialSearch/SequentialSearch/源.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int SequentialSearch(int n, int s[], int key);
+int SequentialSearch(int n, int s[], int key) {
+	int i;
+	i = 0;
+	while (i < n && s[i] != key)
+		i++;
+	if (s[i] == key)
+		return i;
+	else
+		return 0;
+}
+int main() {
+	int T[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int n = 10, x = 30;
+	printf("%d", SequentialSearch(9,T,x));
+	return 0;
+}
